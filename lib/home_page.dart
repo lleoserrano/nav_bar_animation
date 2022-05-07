ce6c22a4895f37332692b6714efcbb0343455c83
Leonardo Serrano <dev.leonardoserrano@gmail.com>
2022-05-07 10:16:38 +0100

    New nav bar
    New Version nav bar model.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nav_bar_animation/nav_bar_two.dart';
 
 import 'home_controller.dart';
 
@@ -15,129 +16,138 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final _ = Get.put(HomeController());
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Olá'),
-      ),
-      extendBody: true,
-      bottomNavigationBar: Obx(
-        () => AnimatedContainer(
-          duration: const Duration(milliseconds: 500),
-          color:
-              _.indexSelected.value % 2 == 0 ? Colors.white : Colors.blue[900],
-          child: Theme(
-            data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
-            child: BottomNavigationBar(
-              backgroundColor: Colors.transparent,
-              currentIndex: _.indexSelected.value,
-              selectedItemColor: Colors.white,
-              onTap: (index) {
-                _.indexSelected.value = index;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Olá'),
+        ),
+        extendBody: true,
+        bottomNavigationBar: Obx(
+          () => AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            color: _.indexSelected.value % 2 == 0
+                ? Colors.white
+                : Colors.blue[900],
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+              child: BottomNavigationBar(
+                backgroundColor: Colors.transparent,
+                currentIndex: _.indexSelected.value,
+                selectedItemColor: Colors.white,
+                onTap: (index) {
+                  _.indexSelected.value = index;
 
-                print(_.indexSelected.value % 2);
-              },
-              items: [
-                BottomNavigationBarItem(
-                  icon: Column(
-                    children: [
-                      const Icon(Icons.home),
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        width: _.indexSelected.value == 0 ? 100 : 0,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: _.indexSelected.value == 0
-                                ? BorderSide(
-                                    width: 2,
-                                    color: Colors.lightBlue.shade600,
-                                  )
-                                : BorderSide.none,
+                  print(_.indexSelected.value % 2);
+                },
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.home),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: _.indexSelected.value == 0 ? 100 : 0,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: _.indexSelected.value == 0
+                                  ? BorderSide(
+                                      width: 2,
+                                      color: Colors.lightBlue.shade600,
+                                    )
+                                  : BorderSide.none,
+                            ),
                           ),
+                          child: const Center(child: Text('Home')),
                         ),
-                        child: const Center(child: Text('Home')),
-                      ),
-                    ],
+                      ],
+                    ),
+                    label: '',
                   ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Column(
-                    children: [
-                      const Icon(Icons.explore),
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        width: _.indexSelected.value == 1 ? 100 : 0,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: _.indexSelected.value == 1
-                                ? BorderSide(
-                                    width: 2,
-                                    color: Colors.lightBlue.shade600,
-                                  )
-                                : BorderSide.none,
+                  BottomNavigationBarItem(
+                    icon: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.explore),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: _.indexSelected.value == 1 ? 100 : 0,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: _.indexSelected.value == 1
+                                  ? BorderSide(
+                                      width: 2,
+                                      color: Colors.lightBlue.shade600,
+                                    )
+                                  : BorderSide.none,
+                            ),
                           ),
+                          child: const Center(child: Text('Explorer')),
                         ),
-                        child: const Center(child: Text('Explorer')),
-                      ),
-                    ],
+                      ],
+                    ),
+                    label: '',
                   ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Column(
-                    children: [
-                      const Icon(Icons.card_giftcard),
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        width: _.indexSelected.value == 2 ? 100 : 0,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: _.indexSelected.value == 2
-                                ? BorderSide(
-                                    width: 2,
-                                    color: Colors.lightBlue.shade600,
-                                  )
-                                : BorderSide.none,
+                  BottomNavigationBarItem(
+                    icon: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.card_giftcard),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: _.indexSelected.value == 2 ? 100 : 0,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: _.indexSelected.value == 2
+                                  ? BorderSide(
+                                      width: 2,
+                                      color: Colors.lightBlue.shade600,
+                                    )
+                                  : BorderSide.none,
+                            ),
                           ),
+                          child: const Center(child: Text('Catalogo')),
                         ),
-                        child: const Center(child: Text('Catalogo')),
-                      ),
-                    ],
+                      ],
+                    ),
+                    label: '',
                   ),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                  icon: Column(
-                    children: [
-                      const Icon(Icons.settings),
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 500),
-                        width: _.indexSelected.value == 3 ? 100 : 0,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: _.indexSelected.value == 3
-                                ? BorderSide(
-                                    width: 2,
-                                    color: Colors.lightBlue.shade600,
-                                  )
-                                : BorderSide.none,
+                  BottomNavigationBarItem(
+                    icon: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.settings),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: _.indexSelected.value == 3 ? 100 : 0,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: _.indexSelected.value == 3
+                                  ? BorderSide(
+                                      width: 2,
+                                      color: Colors.lightBlue.shade600,
+                                    )
+                                  : BorderSide.none,
+                            ),
                           ),
+                          child: const Center(child: Text('Settings')),
                         ),
-                        child: const Center(child: Text('Settings')),
-                      ),
-                    ],
+                      ],
+                    ),
+                    label: '',
                   ),
-                  label: '',
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {},
-          child: const Text('LS-Dev'),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              Get.to(() => const NavBarTwo());
+            },
+            child: const Text('LS-Dev'),
+          ),
         ),
       ),
     );
